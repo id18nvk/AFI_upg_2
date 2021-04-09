dab3c84a9c20e931bf1092ad637f7d958737d8ed
Nike Starbrink <nike.starbrink@hotmail.com>
2021-04-09 13:38:19 +0200

    my model

---
+++ AFI_upg_2/Models/PrenumerantDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrenumerantSystem.Models
+{
+    public class PrenumerantDetails
+    {
+        public PrenumerantDetails(){}
+
+        public int pr_Id { get; set; }
+        public string pr_Tlfnr { get; set; }
+        public string pr_Firstname { get; set; }
+        public string pr_Lastname { get; set; }
+    }
+}
